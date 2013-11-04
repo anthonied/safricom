09f5a57af90ce75ba2c3597838902e20c7616118
Louisa Hattingh <louisah@solutionserver.co.za>
2013-11-04 12:10:21 +0200

    First layout

---
+++ Safricom/Scripts/Custom/Custom.js
@@ -0,0 +1,17 @@
+﻿navbarFocus = function (menuItem)
+{
+	var item = menuItem;
+	alert(item);
+};
+
+IsValidEmailAddress1 = function (email)
+{
+	var filter = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+	return filter.test(email);
+};
+
+IsValidEmailAddress2 = function (email)
+{
+	var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	return filter.test(email);
+};
---
+++ Safricom/ScriptsTests/Custom/Custom.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="../qunit-1.12.0.js" />
+
+test("Evaluate email address", 1, function ()
+{
+	ok(IsValidEmailAddress1("a"), "'a' is not a vailid email address");
+});
